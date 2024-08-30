b118312a97fd3149057950f47bfb132f576bd3d4
Om Nimbalkar2004 <ompnimbalkar2004@gmail.com>
2024-08-30 16:57:26 +0530

    Updated backend and frontend link

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,29 +1,4 @@
 /* eslint-disable no-undef */
-// const express = require("express");
-// const todoRoutes = require("./routes/todoRoutes");
-
-
-// const app = express();
-
-// //Middlewere
-// app.use(express.json());
-
-
-
-// app.get('/', (req, res) => {
-//   res.send('Welcome to the Todo API');
-// });
-
-// // todo routes
-
-// app.use("/api" , todoRoutes);
-
-
-// // Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -36,15 +11,9 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = ['http://localhost:5173/Todo-list.github.io/', 'https://omnimbalkar04.github.io/Todo-list.github.io/'];
+
 app.use(cors({
-    origin: function (origin, callback) {
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
+    origin: "*",
 }));
 
 app.use(express.json()); // Middleware to parse JSON request bodies
